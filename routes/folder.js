9e88551b49774c630f309e74ee3621af12709797
nghervella <nghervella@esei.uvigo.es>
2019-12-05 16:19:05 +0100

    Crear carpetas

--- routes/folder.js
+++ routes/folder.js
@@ -47,6 +47,73 @@ router.get('/:id', function (req, res, next) {
   });
 });
 
+router.post('/create/:id', function (req, res, next) {
+  const token = req["headers"]["authorization"].split(" ")[1];
+  
+// console.log(token);
+  nJwt.verify(token, secretKey, function (err, verifiedJwt) {
+    if (err) {
+      console.log(err)
+      res.status(401);
+      res.json({
+        "status": 401,
+        "error": err,
+        "response": null
+      });
+    } else {
+      let subquery='';
+      if(req.params.id === 'null'){
+        res.locals.connection.query('INSERT INTO CARPETA(NOMBRE, PROPIETARIO) values (?,?)',
+        [req.body.NOMBRE, verifiedJwt["body"]["idUser"]],
+        function (error, results) {
+          
+          if (error) {
+            res.status(500);
+            res.json({
+              "status": 500,
+              "error": error,
+              "response": null
+            });
+            //If there is error, we send the error in the error section with 500 status
+          } else {
+            res.json({
+              "status": 201,
+              "error": null,
+              "response": results
+            });
+            console.log("creada");
+            //If there is no error, all is good and response is 200OK.
+          }
+        });
+      }else{
+        res.locals.connection.query('INSERT INTO CARPETA(NOMBRE, PROPIETARIO, PADRE) values (?,?,?)',
+        [req.body.NOMBRE, verifiedJwt["body"]["idUser"], req.params.id],
+        function (error, results) {
+          
+          if (error) {
+            res.status(500);
+            res.json({
+              "status": 500,
+              "error": error,
+              "response": null
+            });
+            //If there is error, we send the error in the error section with 500 status
+          } else {
+            res.json({
+              "status": 201,
+              "error": null,
+              "response": results
+            });
+            console.log("creada");
+            //If there is no error, all is good and response is 200OK.
+          }
+        });
+      }
+
+    }
+  });
+});
+
 
 
 module.exports = router;
