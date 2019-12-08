3d6b078b61ce72a4baf77036b66ae799f32b567c
nghervella <nghervella@esei.uvigo.es>
2019-12-08 20:42:27 +0100

    Carpeta anterior
    Al acceder a una carpeta, podemos volver a la anterior

--- routes/folder.js
+++ routes/folder.js
@@ -151,8 +151,49 @@ router.post('/delete/', function (req, res, next) {
             //If there is no error, all is good and response is 200OK.
           }
         });
-      
+      }
+  });
+});
 
+router.get('/back/:id', function (req, res, next) {
+  const token = req["headers"]["authorization"].split(" ")[1];
+// console.log(token);
+  nJwt.verify(token, secretKey, function (err, verifiedJwt) {
+    if (err) {
+      console.log(err)
+      res.status(401);
+      res.json({
+        "status": 401,
+        "error": err,
+        "response": null
+      });
+    } else {
+      let subquery='';
+      if(req.params.id === 'null'){
+          subquery = 'padre IS NULL';
+      }else{
+        subquery = 'padre="'+ req.params.id+'"';
+      }
+      console.log(subquery);
+      res.locals.connection.query('SELECT * FROM carpeta WHERE propietario="' + verifiedJwt["body"]["idUser"] + '" AND ID_CARPETA ='+ req.params.id,
+        function (error, results, fields) {
+          if (error) {
+            res.status(500);
+            res.json({
+              "status": 500,
+              "error": error,
+              "response": null
+            });
+            //If there is error, we send the error in the error section with 500 status
+          } else {
+            res.json({
+              "status": 200,
+              "error": null,
+              "response": results
+            });
+            //If there is no error, all is good and response is 200OK.
+          }
+        });
     }
   });
 });
